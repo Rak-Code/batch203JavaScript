16f2f9008e4bc50300f369301137928bf5724036
Rakesh Gupta <rakeshgupta1361411@gmail.com>
2025-02-21 19:45:24 +0530

    done

--- Basics/main.js
+++ Basics/main.js
@@ -1,91 +1,116 @@
-// console.log("Hello")
-// // document.write("Hello world")
+// // console.log("Hello")
+// // // document.write("Hello world")
 
-// if (18 > 15) {
-//     console.log("true");
-// }
-// else{
-//     console.log("false");
-// }
+// // if (18 > 15) {
+// //     console.log("true");
+// // }
+// // else{
+// //     console.log("false");
+// // }
 
-// age = 19
-// tickets = age > 18 ? "yes" : "no"
-// console.log(tickets)
+// // age = 19
+// // tickets = age > 18 ? "yes" : "no"
+// // console.log(tickets)
 
-// a = 10
-// b = 20
-// c = 30
-// if (a > b && a > c) {
-//     console.log("a is greater than b and c");
-// }
-// else{
-//     if(b > c){
-//         console.log("b is greater than c and a");
-//     }
-//     else{
-//         console.log("c is greater than b and a");
-//     }
+// // a = 10
+// // b = 20
+// // c = 30
+// // if (a > b && a > c) {
+// //     console.log("a is greater than b and c");
+// // }
+// // else{
+// //     if(b > c){
+// //         console.log("b is greater than c and a");
+// //     }
+// //     else{
+// //         console.log("c is greater than b and a");
+// //     }
     
-// }
+// // }
 
 
-// for (let i = 0; i <= 5; i++) {
-//     console.log(i);
+// // for (let i = 0; i <= 5; i++) {
+// //     console.log(i);
     
     
-// }
+// // }
 
-// console.log("2" == 2);
-// console.log("2" === 2);
+// // console.log("2" == 2);
+// // console.log("2" === 2);
 
-// a = ["start",3,"hello",56,14,]
+// // a = ["start",3,"hello",56,14,]
 
-// for(let i = 0; i < a.length; i++){ //mostly used 
-//     console.log(a[i]);
-// }
+// // for(let i = 0; i < a.length; i++){ //mostly used 
+// //     console.log(a[i]);
+// // }
 
-// for (const i in a) {  //index return useful in objects return key.
-//    console.log(i);
+// // for (const i in a) {  //index return useful in objects return key.
+// //    console.log(i);
    
+// // }
+
+// // for (const i of a) { //arrays or strings return value 
+// //     console.log(i);
+    
+// // }
+
+// // a.forEach(i => {  //arrays return value
+// //     console.log(i);
+    
+// // });
+
+// i = 1;
+// do {
+//     console.log(i);
+//     i++;
+    
+// } while (i < 3);
+
+// while(i < 5){
+//     console.log(i);
+//     i++;
 // }
 
-// for (const i of a) { //arrays or strings return value 
-//     console.log(i);
-    
+// function greet(name){
+//     console.log(`Hello ${name}`);
 // }
 
-// a.forEach(i => {  //arrays return value
-//     console.log(i);
-    
-// });
+// greet("Igris")
 
-i = 1;
-do {
-    console.log(i);
-    i++;
-    
-} while (i < 3);
+// function greet(name = "Igris"){
+//     console.log(`Hello ${name}`);
+//     }
 
-while(i < 5){
-    console.log(i);
-    i++;
-}
+//     let ab = function(a,b){
+//         return a + b;
+//     }
 
-function greet(name){
-    console.log(`Hello ${name}`);
-}
+// let ac = (a,b) => {
+//     return a + b
+// }
+// console.log(ac);
 
-greet("Igris")
+//callabcks
 
-function greet(name = "Igris"){
-    console.log(`Hello ${name}`);
+function createObject(){
+    return{
+        name: "",
+        getName: function(){ return this.name},
+        setName: function(name)  { return  this.name = name}
+        
+        
     }
-
-    let ab = function(a,b){
-        return a + b;
-    }
-
-let ac = (a,b) => {
-    return a + b
 }
-console.log(ac);
+
+Beru = createObject()
+Beru.setName("Beru")
+console.log(Beru.getName());
+
+Igris = createObject()
+Igris.setName("Igris")
+console.log(Igris.getName(),Beru.getName());
+
+
+
+
+
